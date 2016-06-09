1ec7cd980247ff0578300326d665acbe8362868a
Kshitij Garg <garginator@gmail.com>
2016-06-09 16:10:22 +0530

    Improved the Tic Tac Toe Program - zc.py ; Works fine . Need to add square override protect.

--- zc.py
+++ zc.py
@@ -1,34 +1,51 @@
 import sys
 class Grid(object):
+
 	def __init__(self):
 		self.grid =['1','2','3','4','5','6','7','8','9']
 	
 	def changeSquare(self,block,player):
-		if(player == 1):
+		if(player == 0):
 				self.grid[ block-1 ] = "x"
 		else:
 				self.grid[ block-1 ] = "o"
 	
 	def printBoard(self):
+		print '\n\n\n'
 		print self.grid[0],self.grid[1],self.grid[2]
 		print self.grid[3],self.grid[4],self.grid[5]
 		print self.grid[6],self.grid[7],self.grid[8]
+		print '\n\n\n'
+	
+	def checkBoard(self):
+		status = False
+		if ( self.grid[0] == self.grid[1] == self.grid[2] ) | (self.grid[3] == self.grid[4] == self.grid[5]) | (self.grid[6] == self.grid[7] == self.grid[8]) :
+			status = True
+		elif (self.grid[0] == self.grid[3] == self.grid[6]) | (self.grid[1] == self.grid[4] == self.grid[7]) | (self.grid[2] == self.grid[5] == self.grid[8]) :
+			status = True
+		elif(self.grid[0]==self.grid[4]==self.grid[8]) | (self.grid[2]==self.grid[4]==self.grid[6]) :
+			status = True
+		return status
+def sysExit():
+	sys.exit()
+
+def playerWin(number):
+        print 'Congratulations !! Player ' , number+1 , 'WINS'
+	sysExit()
 
 while True:
-	choice = input("Enter 1 to play a new game or 2 to exit")
+	choice = input("Enter 1 to play a new game or 2 to exit\n")
 	if choice == 1:
 		grid = Grid()
 		for i in range(9) :
-			square = input(" Enter the Square Number ")
-			if i%2 == 0:
-				grid.changeSquare(square,1)
-			else:
-				grid.changeSquare(square,2)
-			
-			print "After Chance ",i+1," the Board now looks like "
+			square = input("Enter the Square Number\n")
+			grid.changeSquare(square,i%2)
 			grid.printBoard()
-	if choice == 2:
-		print "Final Board is "
-		grid.printBoard()
-		print "Thank you for Playing "
-		sys.exit()
+			result = grid.checkBoard()
+			if result :
+				playerWin(i%2)
+		print '\n\nThe game is tied ! Try Again\n\n'
+	else :
+		print '\n\nExiting the Program\n\n'
+		sysExit()
+
